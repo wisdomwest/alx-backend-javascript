316af1d367a078957960df7fd38ff1a3897c0247
wisdomwest <harmonykoome@gmail.com>
2024-03-07 20:31:33 +0300

    i think this is a better implementation

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,11 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== 'function') {
-        throw new Error(
-          'Class extending Building must override evacuationWarningMessage',
-        );
-      }
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this.sqft = sqft;
   }
