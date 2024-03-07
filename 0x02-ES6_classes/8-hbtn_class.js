8b3f1617307ee7746ac5aa772e0440d16925fa92
wisdomwest <harmonykoome@gmail.com>
2024-03-07 20:18:38 +0300

    task 8 Primitive - Holberton Class class cast to

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -20,9 +20,9 @@ export default class HolbertonClass {
     return this._location;
   }
 
-  [Symbol.toPrimitive](hint) {
-    if (hint === 'string') return this.location;
-    if (hint === 'number') return this.size;
+  [Symbol.toPrimitive](cast) {
+    if (cast === 'string') return this.location;
+    if (cast === 'number') return this.size;
     return this;
   }
 }
