1816f63ddce4e071247c476b67e46cf0b48b79c0
wisdomwest <harmonykoome@gmail.com>
2024-03-13 17:51:30 +0300

    task 3

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,12 @@ export interface Teacher {
 export interface Directors extends Teacher {
   numberOfReports: number,
 }
+
+export interface printTeacherFunction {
+  (firstName: string, lastName: string): string,
+}
+
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
