6c5b26f53d4934baa635d604fd91ac96a1b7b339
wisdomwest <harmonykoome@gmail.com>
2024-04-29 22:52:24 +0300

    okay please work

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,10 +1,5 @@
-/**
- * This function displays a message in the console
- * @param {string} message - The message to display
- * @returns {void}
- */
 const displayMessage = (message) => {
     console.log(message);
-}
+};
 
 module.exports = displayMessage;
