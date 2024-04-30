652161e4c8ac6723d201f4b81dc63bca7797f0e5
wisdomwest <harmonykoome@gmail.com>
2024-04-30 17:55:51 +0300

    i think error handling issue

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,42 +7,47 @@ const app = http.createServer();
 const dataPath = process.argv.length > 2 ? process.argv[2] : '';
 
 const countStudents = (dataPath) => new Promise((resolve, reject) => {
-  fs.readFile(dataPath, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the database'));
-    }
-    if (data) {
-      const report = [];
-      const fileLines = data.trim().split('\n');
-      const studentGroups = {};
-      const dbFieldNames = fileLines[0].split(',');
-      const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
-
-      for (const line of fileLines.slice(1)) {
-        const studentRecord = line.split(',');
-        const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-        const field = studentRecord[studentRecord.length - 1];
-        if (!studentGroups[field]) {
-          studentGroups[field] = [];
-        }
-        const student = {};
-        for (let i = 0; i < studentPropNames.length; i += 1) {
-          student[studentPropNames[i]] = studentPropValues[i];
-        }
-        studentGroups[field].push(student);
+  if (!dataPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dataPath) {
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
       }
+      if (data) {
+        const report = [];
+        const fileLines = data.trim().split('\n');
+        const studentGroups = {};
+        const dbFieldNames = fileLines[0].split(',');
+        const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
-      const totalStudents = Object.values(studentGroups)
-        .reduce((total, group) => total + group.length, 0);
-      report.push(`Number of students: ${totalStudents}`);
-      for (const [field, students] of Object.entries(studentGroups)) {
-        if (students.length > 0) {
-          report.push(`Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.firstname).join(', ')}`);
+        for (const line of fileLines.slice(1)) {
+          const studentRecord = line.split(',');
+          const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
+          const field = studentRecord[studentRecord.length - 1];
+          if (!studentGroups[field]) {
+            studentGroups[field] = [];
+          }
+          const student = {};
+          for (let i = 0; i < studentPropNames.length; i += 1) {
+            student[studentPropNames[i]] = studentPropValues[i];
+          }
+          studentGroups[field].push(student);
         }
+
+        const totalStudents = Object.values(studentGroups)
+          .reduce((total, group) => total + group.length, 0);
+        report.push(`Number of students: ${totalStudents}`);
+        for (const [field, students] of Object.entries(studentGroups)) {
+          if (students.length > 0) {
+            report.push(`Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.firstname).join(', ')}`);
+          }
+        }
+        resolve(report.join('\n'));
       }
-      resolve(report.join('\n'));
-    }
-  });
+    });
+  }
 });
 
 const server = [
