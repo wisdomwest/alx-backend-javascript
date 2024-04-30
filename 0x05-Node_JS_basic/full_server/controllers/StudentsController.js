85546c3b963f87925293a3b4b496b59154370562
wisdomwest <harmonykoome@gmail.com>
2024-04-30 20:35:37 +0300

    okay i think i figured it out

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -6,8 +6,8 @@ const dataPath = process.argv[2];
 class StudentsController {
   static getAllStudents(request, response) {
     readDatabase(dataPath)
-      .then((studentGroups) => {
-        const data = ['This is the list of our students'];
+      .then((fields) => {
+        let responseText = '';
 
         const arrange = (a, b) => {
           if (a[0].toLowerCase() < b[0].toLowerCase()) return -1;
@@ -15,34 +15,39 @@ class StudentsController {
           return 0;
         };
 
-        for (const [field, students] of Object.entries(studentGroups).sort(arrange)) {
-          data.push(`Number of students in ${field}: ${students.length}. List: ${students.map((student) => student.firstname).join(', ')}`);
-        }
-        response.status(200).send(data.join('\n'));
-      })
-      .catch((error) => {
-        response.status(500).send(error instanceof Error ? error.message : error.toString());
-      });
+        for (const field in fields) {
+          if (Object.hasOwnProperty.call(fields, field)) {
+            const sortedNames = fields[field].sort(arrange);
+            const names = sortedNames.join(', ');
+            const count = sortedNames.length;
+
+            responseText += `\nNumber of students in ${field}: ${count}. List: ${names}`;
+      }
+    }
+
+    response.status(200).send(`This is the list of our students\n${responseText}`);
+    })
+    .catch((error) => response.status(500).send(`This is the list of our students\n${error.message}`));
   }
 
   static getAllStudentsByMajor(request, response) {
     const { major } = request.params;
+
     if (!MAJORS.includes(major)) {
       response.status(500).send('Major parameter must be CS or SWE');
-    } else {
-      readDatabase(dataPath)
-        .then((studentGroups) => {
-          let data = '';
-
-          if (studentGroups[major]) {
-            data = `List: ${studentGroups[major].map((student) => student.firstname).join(', ')}`;
-          }
-          response.status(200).send(data);
-        })
-        .catch((error) => {
-          response.status(500).send(error instanceof Error ? error.message : error.toString());
-        });
+      return;
     }
+
+    readDatabase(dataPath)
+      .then((fields) => {
+        const students = fields[major];
+        const names = students.join(', ');
+        const count = students.length;
+
+        response.status(200).send(`List: ${names}`);
+      })
+      .catch((error) => response.status(500).send(`List: ${error.message}`));
+
   }
 }
 
--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,37 +1,32 @@
 import fs from 'fs';
 
-const readDatabase = (path) => new Promise((resolve, reject) => {
-  if (!path) {
-    reject(new Error('Cannot load the database'));
-  }
-  if (path) {
-    fs.readFile(path, 'utf-8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const fileLines = data.trim().split('\n');
-        const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
+const readDatabase = (dataPath) => new Promise((resolve, reject) => {
+  fs.readFile(dataPath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
 
-        for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-          const field = studentRecord[studentRecord.length - 1];
-          if (!studentGroups[field]) {
-            studentGroups[field] = [];
-          }
-          const student = {};
-          for (let i = 0; i < studentPropNames.length; i += 1) {
-            student[studentPropNames[i]] = studentPropValues[i];
-          }
-          studentGroups[field].push(student);
+    if (data) {
+      const lines = data.split('\n').filter((line) => line !== '').slice(1);
+      const students = lines.map((line) => line.split(','));
+
+      const fields = {};
+
+      students.forEach((student) => {
+        const firstName = student[0];
+        const field = student[3];
+
+        if (!fields[field]) {
+          fields[field] = [firstName];
+        } else {
+          fields[field].push(firstName);
         }
-        resolve(studentGroups);
-      }
-    })
-  }
+      });
+
+      resolve(fields);
+    }
+  });
 });
 
 export default readDatabase;
